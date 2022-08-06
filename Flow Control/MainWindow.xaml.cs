3e8a481f0fd2ebd5fc9bf531b621f486d934f36f
JamesCJ60 <jamescraddockjones@icloud.com>
2022-08-06 23:12:36 +0100

    Added device checker on start up

--- Flow Control/MainWindow.xaml.cs
+++ Flow Control/MainWindow.xaml.cs
@@ -7,11 +7,13 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Flow_Control.Scripts;
 using Interop;
 
 namespace Flow_Control
@@ -25,7 +27,29 @@ namespace Flow_Control
         {
             InitializeComponent();
 
+            compatCheck();
+            
+
             PagesNavigation.Navigate(new System.Uri("Pages/Home.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        public void compatCheck()
+        {
+            string deviceName = MotherboardInfo.Product;
+
+            if (!deviceName.Contains("Flow"))
+            {
+                System.Windows.Forms.DialogResult dialog = System.Windows.Forms.MessageBox.Show("Unsupported device detected! This program only works on ROG Flow devices", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                if (dialog == System.Windows.Forms.DialogResult.OK)
+                {
+                    Environment.Exit(0);
+
+                }
+                else if (dialog == System.Windows.Forms.DialogResult.None)
+                {
+                    Environment.Exit(0);
+                }
+            }
         }
     }
 }
