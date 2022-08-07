98293728fde2fd1764f5f23aca639266294a30d1
JamesCJ60 <jamescraddockjones@icloud.com>
2022-08-07 13:53:15 +0100

    Add home menu system spec display

--- Flow Control/Pages/Home.xaml.cs
+++ Flow Control/Pages/Home.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Flow_Control.Scripts;
+using Flow_Control.Properties;
 
 namespace Flow_Control.Pages
 {
@@ -28,6 +29,56 @@ namespace Flow_Control.Pages
             deviceName = deviceName.Substring(0, deviceName.LastIndexOf('_'));
             deviceName = deviceName.Replace("ROG ", null);
             lblDeviceName.Text = deviceName;
+
+
+            lblCPUName.Text = GetSystemInfo.GetCPUName().Replace("with Radeon Graphics", null); ;
+            lbliGPUName.Text = GetSystemInfo.GetiGPUName().Replace("(R)", null); ;
+            lbldGPUName.Text = GetSystemInfo.GetdGPUName().Replace("Laptop GPU", null); ;
+
+            if(GetSystemInfo.GetdGPUName() == null || GetSystemInfo.GetdGPUName() == "")
+            {
+                dGPUName.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        public void switchProfile(int ACProfile)
+        {
+            if (ACProfile == 0)
+            {
+                imgACProfile.Source = new BitmapImage(new Uri("pack://application:,,,/Assets/ACProfiles/Silent.png"));
+            }
+            if (ACProfile == 1)
+            {
+                imgACProfile.Source = new BitmapImage(new Uri("pack://application:,,,/Assets/ACProfiles/Bal.png"));
+            }
+            if (ACProfile == 2)
+            {
+                imgACProfile.Source = new BitmapImage(new Uri("pack://application:,,,/Assets/ACProfiles/Turbo.png"));
+            }
+            if (ACProfile == 3)
+            {
+                imgACProfile.Source = new BitmapImage(new Uri("pack://application:,,,/Assets/ACProfiles/Windows.png"));
+            }
+        }
+
+        private void rdSilent_Click(object sender, RoutedEventArgs e)
+        {
+            switchProfile(0);
+        }
+
+        private void rdPerf_Click(object sender, RoutedEventArgs e)
+        {
+            switchProfile(1);
+        }
+
+        private void rdTurbo_Click(object sender, RoutedEventArgs e)
+        {
+            switchProfile(2);
+        }
+
+        private void rdMan_Click(object sender, RoutedEventArgs e)
+        {
+            switchProfile(3);
         }
     }
 }
---
+++ Flow Control/Scripts/GetSystemInfo.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibreHardwareMonitor;
+using LibreHardwareMonitor.Hardware;
+
+namespace Flow_Control.Scripts
+{
+    internal class GetSystemInfo
+    {
+        public static Computer thisPC;
+        public class UpdateVisitor : IVisitor
+        {
+            public void VisitComputer(IComputer computer)
+            {
+                computer.Traverse(this);
+            }
+            public void VisitHardware(IHardware hardware)
+            {
+                hardware.Update();
+                foreach (IHardware subHardware in hardware.SubHardware) subHardware.Accept(this);
+            }
+            public void VisitSensor(ISensor sensor) { }
+            public void VisitParameter(IParameter parameter) { }
+        }
+
+
+        public static string GetCPUName()
+        {
+            thisPC = new Computer()
+            {
+                IsCpuEnabled = true,
+                IsGpuEnabled = true,
+                IsMemoryEnabled = true,
+                IsBatteryEnabled = true,
+                IsPsuEnabled = true,
+                IsControllerEnabled = true,
+                IsMotherboardEnabled = true
+            };
+            thisPC.Open();
+            thisPC.Accept(new UpdateVisitor());
+
+            try
+            {
+                foreach (var hardware in thisPC.Hardware)
+                {
+                    hardware.Update();
+                    if (hardware.HardwareType == HardwareType.Cpu)
+                    {
+                        foreach (var sensor in hardware.Sensors)
+                        {
+                            return hardware.Name;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) { }
+
+            return "";
+        }
+
+        public static string GetiGPUName()
+        {
+            thisPC = new Computer()
+            {
+                IsCpuEnabled = true,
+                IsGpuEnabled = true,
+                IsMemoryEnabled = true,
+                IsBatteryEnabled = true,
+                IsPsuEnabled = true,
+                IsControllerEnabled = true,
+                IsMotherboardEnabled = true
+            };
+            thisPC.Open();
+            thisPC.Accept(new UpdateVisitor());
+
+            try
+            {
+                foreach (var hardware in thisPC.Hardware)
+                {
+                    hardware.Update();
+                    if (hardware.HardwareType == HardwareType.GpuAmd)
+                    {
+                        foreach (var sensor in hardware.Sensors)
+                        {
+
+                            if (!hardware.Name.Contains("56") || !hardware.Name.Contains("64"))
+                            {
+                                if (hardware.Name.Contains("Vega") && hardware.Name.Contains("3") || hardware.Name.Contains("Vega") && hardware.Name.Contains("6") || hardware.Name.Contains("Vega") && hardware.Name.Contains("8") || hardware.Name.Contains("Vega") && hardware.Name.Contains("9") || hardware.Name.Contains("Vega") && hardware.Name.Contains("10") || hardware.Name.Contains("Vega") && hardware.Name.Contains("11") || hardware.Name == "AMD Radeon Graphics" || hardware.Name == "AMD Radeon(TM) Graphics" || hardware.Name == "AMD Radeon RX Vega Graphics")
+                                {
+                                    return hardware.Name;
+                                }
+                            }
+
+                        }
+                    }
+
+                    if(hardware.HardwareType == HardwareType.GpuIntel)
+                    {
+                        return hardware.Name;
+                    }
+                }
+            }
+            catch (Exception ex) { }
+
+            return "";
+        }
+
+
+        public static string GetdGPUName()
+        {
+            thisPC = new Computer()
+            {
+                IsCpuEnabled = true,
+                IsGpuEnabled = true,
+                IsMemoryEnabled = true,
+                IsBatteryEnabled = true,
+                IsPsuEnabled = true,
+                IsControllerEnabled = true,
+                IsMotherboardEnabled = true
+            };
+            thisPC.Open();
+            thisPC.Accept(new UpdateVisitor());
+
+            try
+            {
+                foreach (var hardware in thisPC.Hardware)
+                {
+                    hardware.Update();
+                    if (hardware.HardwareType == HardwareType.GpuAmd)
+                    {
+                        foreach (var sensor in hardware.Sensors)
+                        {
+
+                            if (!hardware.Name.Contains("56") || !hardware.Name.Contains("64"))
+                            {
+                                if (hardware.Name.Contains("Vega") && hardware.Name.Contains("3") || hardware.Name.Contains("Vega") && hardware.Name.Contains("6") || hardware.Name.Contains("Vega") && hardware.Name.Contains("8") || hardware.Name.Contains("Vega") && hardware.Name.Contains("9") || hardware.Name.Contains("Vega") && hardware.Name.Contains("10") || hardware.Name.Contains("Vega") && hardware.Name.Contains("11") || hardware.Name == "AMD Radeon Graphics" || hardware.Name == "AMD Radeon(TM) Graphics" || hardware.Name == "AMD Radeon RX Vega Graphics")
+                                {
+
+                                }
+                                else
+                                {
+                                    return hardware.Name;
+                                }
+                            }
+                            else
+                            {
+                                return hardware.Name;
+                            }
+                        }
+                    }
+
+                    else if (hardware.HardwareType == HardwareType.GpuNvidia)
+                    {
+                        foreach (var sensor in hardware.Sensors)
+                        {
+                            return hardware.Name;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) { return ""; }
+
+            return "";
+        }
+    }
+}
